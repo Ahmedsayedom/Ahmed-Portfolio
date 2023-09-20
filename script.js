cb598e5697bdfdccaff9378d82f4a7d451fe66e1
Ahmedsayedom <frggtf66@gmail.com>
2023-09-21 01:12:20 +0300

    #

--- script.js
+++ script.js
@@ -108,3 +108,22 @@ function readmore() {
     p.classList.add("visually-hidden")
   }
 }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault()
+
+    const targetId = this.getAttribute("href").substring(1)
+    const targetElement = document.getElementById(targetId)
+
+    if (targetElement) {
+      const offset = parseInt(this.getAttribute("data-offset")) || 0
+      const offsetTop =
+        targetElement.getBoundingClientRect().top + window.scrollY
+
+      window.scrollTo({
+        top: offsetTop - offset,
+        behavior: "smooth",
+      })
+    }
+  })
+})
